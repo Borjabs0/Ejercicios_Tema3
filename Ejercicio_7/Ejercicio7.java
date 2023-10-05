6208b6acb0ceac6d623c3800c50671e92de8a373
Borjabs0 <146438165+Borjabs0@users.noreply.github.com>
2023-10-05 17:23:23 +0200

    Add files via upload

---
+++ Ejercicio_7/Ejercicio7.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Ejercicio7 {
+
+    public static void main(String[] args) {
+        Scanner lector = new Scanner(System.in);
+
+        int edad;
+        boolean socioPadre;
+
+        System.out.println("Ingrese su edad: ");
+        edad = lector.nextInt();
+
+        System.out.println("¿Los padres son socios? (true/false) ");
+        socioPadre = lector.nextBoolean();
+
+        lector.close();
+
+        double cuotaBase = 500;
+
+        // Aplicamos el descuento correspondiente
+        if (edad >= 65) {
+            cuotaBase = cuotaBase * 0.5;
+        } else if (edad < 18 && !socioPadre) {
+            cuotaBase = cuotaBase * 0.75;
+        } else if (edad < 18 && socioPadre) {
+            cuotaBase = cuotaBase * 0.65;
+        }
+
+        // Mostramos la cuota a abonar
+        System.out.println("La cuota a abonar es de " + cuotaBase + " €");
+    }
+}
